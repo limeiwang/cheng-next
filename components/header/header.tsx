c7a0f2b74152de238d527e28d920052c1adfc88f
李美旺 <v_limeiwang@duxiaoman.com>
2020-12-31 19:18:28 +0800

    add header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'
+import style from './header.module.css'
+import React, { useState } from 'react'
+
 function Header() {
+	const [flag, setFlag] = useState(false);
+
 	return (
-		<header className="global-header opaque">
+		<header className={`${style['global_header']} opaque`}>
 			<div className="global-header__wrapper">
 				<div className="global-header__large-screen position-relative d-lg-flex justify-content-between d-none w-100 h-100">
 					<Link href='/'><a>Logo</a></Link>
@@ -11,13 +16,17 @@ function Header() {
 					</div>
 				</div>
 				<div className="global-header__small-screen position-relative d-lg-none d-flex justify-content-center w-100 h-100">
-					<div className="global-header__menu-toggle">
+					<div onClick={() => setFlag(!flag)} className="global-header__menu-toggle">
 						<span></span>
 						<span></span>
 						<span></span>
+						<span className="btn btn-warning">按钮</span>
 					</div>
 					<Link href='/'><a>Logo</a></Link>
 					<div className="global-header__account">i</div>
+					<div style={flag ? {} : {display: 'none'}}>
+						flag
+					</div>
 				</div>
 			</div>
 		</header>
