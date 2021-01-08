4bcca0143e2dc1ae6e603c87166edba30533c7ae
李美旺 <17803125899@163.com>
2021-01-08 23:48:25 +0800

    footer jsx

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,24 +1,55 @@
 import './footer.module.less'
 function Footer() {
+	let list = [{
+		label: '中国大陆',
+		value: ''
+	}, {
+		label: '订阅电子报',
+		value: ''
+	}, {
+		label: '联系我们',
+		value: ''
+	}, {
+		label: '应用程序',
+		value: ''
+	}, {
+		label: '关注我们',
+		value: ''
+	}, {
+		label: '法律声明和隐私条款',
+		value: ''
+	}, {
+		label: '沪ICP备09069024号',
+		value: ''
+	}, {
+		label: '沪ICP备09069024号',
+		value: ''
+	}, {
+		label: '电子营业执照',
+		value: ''
+	}, {
+		label: '出版物经营许可证',
+		value: ''
+	}, {
+		label: '沪公网安备',
+		value: ''
+	}, {
+		label: '招聘',
+		value: ''
+	}, {
+		label: '网站地图',
+		value: ''
+	}]
 	return (
 		<footer className="global-footer">
 			<div className="container">
-				{/* <div className="breadcrumb">footer</div> */}
 				<div className="navigation">
 					<ul>
-						<li><a href="">中国大陆</a></li>
-						<li><a href="">订阅电子报</a></li>
-						<li><a href="">联系我们</a></li>
-						<li><a href="">应用程序</a></li>
-						<li><a href="">关注我们</a></li>
-						<li><a href="">法律声明和隐私条款</a></li>
-						<li><a href="">沪ICP备09069024号</a></li>
-						<li><a href="">沪ICP备09069024号</a></li>
-						<li><a href="">电子营业执照</a></li>
-						<li><a href="">出版物经营许可证</a></li>
-						<li><a href="">沪公网安备</a></li>
-						<li><a href="">招聘</a></li>
-						<li><a href="">网站地图</a></li>
+						{
+							list.map(item => {
+								return <li><a href={item.value}>{item.label}</a></li>
+							})
+						}
 					</ul>
 				</div>
 			</div>
